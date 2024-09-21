c5aedf3cb45ada0b928e9e4ce16ef12ab96a70b5
ANAS <anaspappadan@gmail.com>
2024-09-21 16:22:26 +0530

    Refactor: Update styles of navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,10 +10,10 @@ const menuItems = [
 
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
 
   useEffect(() => {
     function handleScroll() {
-      console.log(window.scrollY);
       if (window.scrollY > 100) {
         setIsScrolled(true);
       } else {
@@ -25,6 +25,30 @@ export default function Navbar() {
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const sections = document.querySelectorAll("section"); // Select all sections by their tag or you can use specific class names
+    const options = {
+      root: null, // Uses the viewport as the root
+      threshold: 0.6, // Trigger when 60% of the section is visible
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id); // Set the active section based on the ID of the visible section
+        }
+      });
+    }, options);
+
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+
+    return () => {
+      sections.forEach((section) => observer.unobserve(section));
     };
   }, []);
 
@@ -43,6 +67,18 @@ export default function Navbar() {
       </p>
       <ul className="flex gap-8 text-lg uppercase font-semibold cursor-pointer">
         <li
+          className={
+            `relative hover:text-violet-400 cursor-pointer transition-all
+                ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute 
+                before:bg-violet-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] 
+                before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 
+                after:absolute after:bg-violet-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] 
+                after:bottom-0 after:right-[50%]
+                ` +
+            (activeSection === "home"
+              ? "text-violet-300 before:w-[50%] after:w-[50%] "
+              : "text-white")
+          }
           onClick={() => {
             window.scrollTo({ top: 0, behavior: "smooth" });
             window.history.pushState({}, "", "/");
@@ -51,13 +87,18 @@ export default function Navbar() {
         </li>
         {menuItems.map((item) => (
           <li
-            className={`relative hover:text-gray-400 cursor-pointer transition-all
+            className={
+              `relative hover:text-violet-400 cursor-pointer transition-all
               ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute 
-              before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] 
+              before:bg-violet-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] 
               before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 
-              after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] 
+              after:absolute after:bg-violet-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] 
               after:bottom-0 after:right-[50%]
-              `}
+              ` +
+              (activeSection === item.id
+                ? "text-violet-300 before:w-[50%] after:w-[50%] "
+                : "text-white")
+            }
             key={item.id}>
             <a href={`#${item.id}`}>{item.name}</a>
           </li>
